2a04c4b423cbc7243c201dbd4947498e91a52011
Ib Green <7025232+ibgreen@users.noreply.github.com>
2025-12-18 16:54:09 -0500

    feat(log): Add MemoryLog class (#11)

--- modules/log/src/index.ts
+++ modules/log/src/index.ts
@@ -1,13 +1,18 @@
-import {Log} from './log';
+import {ProbeLog} from './loggers/probe-log';
 
 // DEFAULT EXPORT IS A LOG INSTANCE
-export default new Log({id: '@probe.gl/log'});
+export default new ProbeLog({id: '@probe.gl/log'});
 
 // LOGGING
-export {Log} from './log';
-export {COLOR} from './utils/color';
+export type {Logger} from './loggers/logger';
+export {ProbeLog, ProbeLog as Log} from './loggers/probe-log';
+export {ConsoleLog} from './loggers/console-log';
+export {BaseLog} from './loggers/base-log';
+export type {MemoryLogMessage} from './loggers/memory-log';
+export {MemoryLog} from './loggers/memory-log';
 
 // UTILITIES
+export {COLOR} from './utils/color';
 export {addColor} from './utils/color';
 export {leftPad, rightPad} from './utils/formatters';
 export {autobind} from './utils/autobind';
