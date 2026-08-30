b77cdc0b09de017a4728a9310a281a03c752cbd2
Ib Green <ib@unfolded.ai>
2026-08-30 15:03:13 -0400

    chore: modernize dependencies

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,4 +1,5 @@
 const {getDocusaurusConfig} = require('@vis.gl/docusaurus-website');
+const {resolve} = require('path');
 
 const config = getDocusaurusConfig({
   projectName: 'probe.gl',
@@ -11,7 +12,26 @@ const config = getDocusaurusConfig({
   // examplesDir: './src/examples',
   // exampleTableOfContents: require('./src/examples/table-of-contents.json'),
 
-  search: 'local'
+  search: 'local',
+
+  webpackConfig: {
+    resolve: {
+      alias: {
+        'website-examples': resolve('../examples')
+      }
+    }
+  }
 });
+
+// TODO: Remove this compatibility shim after @vis.gl/docusaurus-website
+// moves onBrokenMarkdownLinks to markdown.hooks.
+config.markdown = {
+  ...config.markdown,
+  hooks: {
+    ...config.markdown?.hooks,
+    onBrokenMarkdownLinks: config.onBrokenMarkdownLinks
+  }
+};
+delete config.onBrokenMarkdownLinks;
 
 module.exports = config;
