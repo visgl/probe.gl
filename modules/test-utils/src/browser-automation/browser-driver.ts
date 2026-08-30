dc425fcdf3e73a4ac2c5c1e3130796ad554a6d57
Ib Green <7025232+ibgreen@users.noreply.github.com>
2026-08-30 15:08:11 -0400

    chore: modernize dependencies (#67)

--- modules/test-utils/src/browser-automation/browser-driver.ts
+++ modules/test-utils/src/browser-automation/browser-driver.ts
@@ -2,7 +2,7 @@
 
 /* eslint-disable no-console */
 
-import puppeteer, {Browser, Page, PuppeteerLaunchOptions, ConsoleMessage} from 'puppeteer';
+import puppeteer, {Browser, Page, ConsoleMessage} from 'puppeteer';
 import ChildProcess from 'child_process';
 
 import {COLOR, Log} from '@probe.gl/log';
@@ -28,6 +28,8 @@ export type ServerConfiguration = {
 };
 
 const AUTO_PORT_START = 5000;
+
+type PuppeteerLaunchOptions = Parameters<typeof puppeteer.launch>[0];
 
 function noop() {} // eslint-disable-line @typescript-eslint/no-empty-function
 
